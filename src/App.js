199057a02a74ed3c6aee7531c92f9f5cb7914ff2
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2024-03-21 15:52:12 -0400

    board created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import * as React from "react";
+import {createBoard} from "./utils/createBoard"
 import './App.css';
 
 function App() {
+ const [board, setBoard] = React.useState(() => createBoard())
+
   return (
     <div>
-     Hello
+     {board.map((row, rowIdx) => (
+      <div key={rowIdx}>{row.map((letter, letterIdx) => {
+        <div key={letterIdx}>Cell</div>
+      })}</div>
+     ))}
     </div>
   );
 }
